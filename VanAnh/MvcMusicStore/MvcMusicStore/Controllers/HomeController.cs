804443b1efbd22c9e91857bb46107159e07fc7a8
Van Anh <vananhdev95@gmail.com>
2019-06-27 15:47:54 +0700

    add Mvc MUsic Store

---
+++ VanAnh/MvcMusicStore/MvcMusicStore/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcMusicStore.Controllers
+{
+    public class HomeController : Controller
+    {
+        //public string Index()
+        //{
+        //    return "nguyễn thị vân anh";
+        //}
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+
+}
